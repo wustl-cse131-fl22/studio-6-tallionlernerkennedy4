e825c65ad9c13b92a66a6197aa4c6732255b5e1a
elana <elana@LAPTOP-V6GU8FUQ.dhcp.wustl.edu>
2022-10-27 12:44:34 -0500

    ;)

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,10 +12,12 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+		if ( n > 0 ) {
+			return 1 / Math.pow(2, n)  + geometricSum(n-1);
+		}
+		else {
+			return 0;	
+		}	
 	}
 
 	/**
@@ -26,14 +28,23 @@ public class RecursiveMethods {
 	 * @param q second operand
 	 * @return greatest common divisor of p and q
 	 */
-	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+	public static int gcd(int p, int q) {		
+		if ( p % q == 0) {
+			return q;
+		}
+		else {
+			int i = q - 1;
+			if (p % i == 0 && q % i == 0) {
+				return i;
+			}
+			else {
+				gcd(p, q-1);
+			}
+		}
+
 	}
 
-	
+
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -42,10 +53,10 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+
+		// FIXME create a helper method that can recursively reverse the given array
+		return new int[0];
+
 	}
 
 	/**
@@ -59,8 +70,21 @@ public class RecursiveMethods {
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
-		
-		// FIXME
-	}
 
+		// FIXME
+		double bound = 5.0;
+		StdDraw.setXscale(-bound,bound);
+		StdDraw.setYscale(-bound,bound);
+
+		if (radius > radiusMinimumDrawingThreshold) {
+		StdDraw.circle(xCenter, yCenter, radius);
+		double oldRadius = radius;
+		radius /= 3;
+		circlesUponCircles(-oldRadius, yCenter, radius, radiusMinimumDrawingThreshold);
+		circlesUponCircles(oldRadius, yCenter, radius, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, -oldRadius, radius, radiusMinimumDrawingThreshold);
+		circlesUponCircles(xCenter, oldRadius, radius, radiusMinimumDrawingThreshold);
+		
+		}
+	}
 }
